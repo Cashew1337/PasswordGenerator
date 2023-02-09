d5a07c806a0dcb9541b8c2e31acd2cf452b86ac2
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-02-08 20:34:55 -0500

    Commit to push updated js after a few hours of coding.

---
+++ README.md
@@ -0,0 +1,3 @@
+https://sabe.io/blog/javascript-yes-no-confirmation-box#:~:text=The%20best%20way%20to%20create,Ok%20and%20a%20Cancel%20button.
+https://stackoverflow.com/questions/59504465/generate-random-password-based-on-user-input-in-javascript
+https://stackoverflow.com/questions/40606697/how-do-i-loop-back-to-the-prompt-if-answer-is-incorrect-javascript
--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -1,6 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar = "!@#$%^&*()";
+var numbers = "0123456789";
+
+var lowerUpper = lowerCase.concat(upperCase);
+var lowerSpecial = lowerCase.concat(specialChar);
+var lowerNumbers = lowerCase.concat(numbers);
+var upperSpecial = upperCase.concat(specialChar);
+var upperNumbers = upperCase.concat(numbers)
+var lowerThree = lowerCase.concat(specialChar, numbers);
+var specialNumbers = specialChar.concat(numbers);
+var upperThree = upperCase.concat(specialChar, numbers)
+var allCharacters = lowerCase.concat(upperCase, specialChar, numbers);
+
+function generatePassword() {
+  var passPrompt = Number(prompt("Password Lenght: 8 - 128"));
+  var charTypeSpecial = confirm("Would you like to include special characters?");
+  var charTypeNumeric = confirm("Would you like to include numbers?");
+  var charTypeUpper = confirm("Would you like to include uppercase letters?");
+  var charTypeLower = confirm("Would you like to include lowercase letters?");
+  
+  var charSet = " ";
+  if (charTypeSpecial && !charTypeNumeric && !charTypeUpper && !charTypeLower) {
+    charSet = specialChar;
+  } else if (!charTypeSpecial && charTypeNumeric && !charTypeUpper && !charTypeLower) {
+    charSet = numbers;
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
