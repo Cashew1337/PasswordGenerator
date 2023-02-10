3193fc8f5363a65e4f4fe697a37d66f5809411b6
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-02-09 20:54:54 -0500

    Commit to push additional changes to JS so as to keep things up to date.

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -18,16 +18,45 @@ var allCharacters = lowerCase.concat(upperCase, specialChar, numbers);
 
 function generatePassword() {
   var passPrompt = Number(prompt("Password Lenght: 8 - 128"));
+  if (!passPrompt) {
+    alert("Please select a password length.");
+    generatePassword();
+  }
   var charTypeSpecial = confirm("Would you like to include special characters?");
   var charTypeNumeric = confirm("Would you like to include numbers?");
   var charTypeUpper = confirm("Would you like to include uppercase letters?");
   var charTypeLower = confirm("Would you like to include lowercase letters?");
   
   var charSet = " ";
-  if (charTypeSpecial && !charTypeNumeric && !charTypeUpper && !charTypeLower) {
+  if (!charTypeSpecial && !charTypeNumeric && !charTypeUpper && !charTypeLower){
+    alert("Please select at least one criteria to generate a password.");
+    generatePassword();
+  } else if (charTypeSpecial && !charTypeNumeric && !charTypeUpper && !charTypeLower) {
     charSet = specialChar;
   } else if (!charTypeSpecial && charTypeNumeric && !charTypeUpper && !charTypeLower) {
     charSet = numbers;
+  } else if (!charTypeSpecial && !charTypeNumeric && !charTypeUpper && charTypeLower) {
+    charSet = lowerCase;
+  } else if (!charTypeSpecial && !charTypeNumeric && charTypeUpper && !charTypeLower) {
+    charSet = upperCase;
+  } else if (charTypeSpecial && charTypeNumeric && !charTypeUpper && !charTypeLower) {
+    charSet = specialNumbers;
+  } else if (charTypeSpecial && !charTypeNumeric && charTypeUpper && !charTypeLower) {
+    charSet = upperSpecial;
+  } else if (charTypeSpecial && !charTypeNumeric && !charTypeUpper && charTypeLower) {
+    charSet = lowerSpecial;
+  } else if (charTypeSpecial && charTypeNumeric && charTypeUpper && !charTypeLower) {
+    charSet = upperThree;
+  } else if (charTypeSpecial && charTypeNumeric && !charTypeUpper && charTypeLower) {
+    charSet = lowerThree;
+  } else {
+    charSet = allCharacters;
+  }
+  var charArray = charSet.split("");
+  var retPass = " ";
+  for(i = 0; i < 0; i++){
+    retPass = charArray.length(Math.floor(Math.random() * passPrompt));
+   return retPass;
   }
 }
 
