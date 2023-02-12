75df318ac7c32bc39ea06d7f7d8bcd3d49f9c7f1
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-02-12 10:13:03 -0500

    Final commit to push completed code to repo after testing.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-https://sabe.io/blog/javascript-yes-no-confirmation-box#:~:text=The%20best%20way%20to%20create,Ok%20and%20a%20Cancel%20button.
-https://stackoverflow.com/questions/59504465/generate-random-password-based-on-user-input-in-javascript
-https://stackoverflow.com/questions/40606697/how-do-i-loop-back-to-the-prompt-if-answer-is-incorrect-javascript
+## Password Generator
+
+# Purpose:
+The purpose of this project was to design an in browser random password generator that would create a password based on user selected criteria.
+
+# Usage:
+To use the app, click the generate password button in the middle of the screen after the page loads.
+After the button is pressed, as series of prompts and confirms will pop up asking for the criteria to be used when generating the password.
+Once the final confirms is selected, the generated password will appear in the box in the middle of the page.
+
+Below is a screenshot of the webpage found at the following link: 
+
+
+
+# Credits
+Confirm refresher(used to refresh on use of confirm() command for use in beginning of password generator function): https://sabe.io/blog/javascript-yes-no-confirmation-box#:~:text=The%20best%20way%20to%20create,Ok%20and%20a%20Cancel%20button.
+Equation(source for initial equation used to try and pull random characters from stored array, replaced with what is currently there after discussion with instructors): https://stackoverflow.com/questions/59504465/generate-random-password-based-on-user-input-in-javascript
+Function Loop(used to figure out how to loop back into the function on instances of incorrect or missing criteria during prompts): https://stackoverflow.com/questions/40606697/how-do-i-loop-back-to-the-prompt-if-answer-is-incorrect-javascript
--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// Variables containing potential password characters.
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var specialChar = "!@#$%^&*()";
 var numbers = "0123456789";
-
+// Variables for possible combinations of potential password characters.
 var lowerUpper = lowerCase.concat(upperCase);
 var lowerSpecial = lowerCase.concat(specialChar);
 var lowerNumbers = lowerCase.concat(numbers);
@@ -13,10 +13,12 @@ var upperSpecial = upperCase.concat(specialChar);
 var upperNumbers = upperCase.concat(numbers)
 var lowerThree = lowerCase.concat(specialChar, numbers);
 var specialNumbers = specialChar.concat(numbers);
-var upperThree = upperCase.concat(specialChar, numbers)
+var upperThree = upperCase.concat(specialChar, numbers);
+var noSpecial = lowerCase.concat(numbers, upperCase);
 var allCharacters = lowerCase.concat(upperCase, specialChar, numbers);
-
+// Function to generate the password, called when pressing the "Generate Password" button.
 function generatePassword() {
+  // Prompts and confirms used to generate the criteria for the password.
   var passPrompt = Number(prompt("Password Lenght: 8 - 128"));
   if (!passPrompt) {
     alert("Please select a password length.");
@@ -27,6 +29,7 @@ function generatePassword() {
   var charTypeUpper = confirm("Would you like to include uppercase letters?");
   var charTypeLower = confirm("Would you like to include lowercase letters?");
   
+  // If statement to help the function determine what character set to use based on user input in the above prompts.
   var charSet = " ";
   if (!charTypeSpecial && !charTypeNumeric && !charTypeUpper && !charTypeLower){
     alert("Please select at least one criteria to generate a password.");
@@ -39,30 +42,41 @@ function generatePassword() {
     charSet = lowerCase;
   } else if (!charTypeSpecial && !charTypeNumeric && charTypeUpper && !charTypeLower) {
     charSet = upperCase;
-  } else if (charTypeSpecial && charTypeNumeric && !charTypeUpper && !charTypeLower) {
+  } else if (charTypeSpecial && charTypeNumeric && !charTypeUpper && !charTypeLower){
     charSet = specialNumbers;
-  } else if (charTypeSpecial && !charTypeNumeric && charTypeUpper && !charTypeLower) {
+  } else if (charTypeSpecial && !charTypeNumeric && charTypeUpper && !charTypeLower){
     charSet = upperSpecial;
-  } else if (charTypeSpecial && !charTypeNumeric && !charTypeUpper && charTypeLower) {
+  } else if (charTypeSpecial && !charTypeNumeric && !charTypeUpper && charTypeLower){
     charSet = lowerSpecial;
   } else if (charTypeSpecial && charTypeNumeric && charTypeUpper && !charTypeLower) {
     charSet = upperThree;
   } else if (charTypeSpecial && charTypeNumeric && !charTypeUpper && charTypeLower) {
     charSet = lowerThree;
+  } else if (!charTypeSpecial && charTypeNumeric && charTypeUpper && charTypeLower) {
+    charSet = noSpecial;
   } else {
     charSet = allCharacters;
   }
+  // Code to create an array from the password critera chosen above.
   var charArray = charSet.split("");
+  console.log(charSet);
+  console.log(charArray);
+  console.log(passPrompt.length)
   var retPass = " ";
-  for(i = 0; i < 0; i++){
-    retPass = charArray.length(Math.floor(Math.random() * passPrompt));
-   return retPass;
+  // Equation to pull random characters from the above array and finish generating the password. Initial equation was similar to the one located at the link in the README labeled Equation. However, after going over the code with Gerson and Eric, my instructor and TA, I was able to edit it and the result is the current equation.
+  for(var i = 0; i < passPrompt; i++){
+    var randomNum = Math.floor(Math.random() * charArray.length);
+    console.log(randomNum)
+    retPass += charArray[randomNum];
   }
+  console.log(retPass);
+  return retPass;
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log({password});
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
